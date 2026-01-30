a022fbdc8b1718b7452fa8fbc1fcf3287de2bb2e
Michel Datskevitch <michel.datskevitch@etu.minesparis.psl.eu>
2026-01-30 14:55:11 +0100

    Last update

--- game/level.py
+++ game/level.py
@@ -1,6 +1,13 @@
 
 from game.Player import Player
 from game.map import Map
+from game.sprites import Sprite
+
+import pygame as pg
+from pygame import Vector2
+
+g = 30 # pesanteur
+acc = Vector2(0, g)
 
 class Level:
     map: Map = None
@@ -8,3 +15,19 @@ class Level:
 
     def __init__(self):
         self.scale = 5
+
+class Projectiles:
+
+    def __init__(self, sprite : Sprite, emitter : Player, position : Vector2, damage : int, speed : Vector2):
+        self.sprite = sprite
+        self.emitter = emitter
+        self.position = position
+        self.damage = damage
+
+    def update_position(self, dt: float) -> Vector2:
+        self.speed += acc*dt
+        self.position += self.speed*dt + acc * (dt ** 2) / 2
+
+        return self.position
+
+    
